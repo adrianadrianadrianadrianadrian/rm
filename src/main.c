0701c2939c265a4690503f0a40ef2ef8c5a41d68
Adrian <a.glouftsis@outlook.com>
2026-08-04 21:28:04 +1000

    remove debug for now.

--- src/main.c
+++ src/main.c
@@ -8,20 +8,6 @@
 #include "lowering/c.h"
 #include <sys/time.h>
 #include <unistd.h>
-#include "debug.h"
-
-struct timeval current_time()
-{
-    struct timeval t;
-    gettimeofday(&t, NULL);
-    return t;
-}
-
-double ms_delta(const struct timeval *t1, const struct timeval *t2)
-{
-    return (t2->tv_sec - t1->tv_sec) * 1000.0 +
-           (t2->tv_usec - t1->tv_usec) / 1000.0;
-}
 
 int compile(char *file_name, struct error *error)
 {
@@ -35,25 +21,11 @@ int compile(char *file_name, struct error *error)
     struct parsed_file parsed = {0};
     struct context c = {0};
 
-    struct timeval start = current_time();
     if (!parse_file(&tb, &parsed, error))     return 0;
-    struct timeval parse_end = current_time();
     if (!contextualise(&parsed, &c, error))   return 0;
-    struct timeval context_end = current_time();
-    show_context(&c);
     if (!soundness_check(&parsed, &c, error)) return 0;
-    struct timeval soundness_end = current_time();
     if (!type_check(&parsed, &c, error))      return 0;
-    struct timeval type_check_end = current_time();
     generate_c(&parsed, &c);
-    struct timeval codegen_end = current_time();
-
-    printf("parsed          %lfms\n", ms_delta(&start, &parse_end));
-    printf("context         %lfms\n", ms_delta(&parse_end, &context_end));
-    printf("soundess        %lfms\n", ms_delta(&context_end, &soundness_end));
-    printf("type_checked    %lfms\n", ms_delta(&soundness_end, &type_check_end));
-    printf("codegen         %lfms\n", ms_delta(&type_check_end, &codegen_end));
-    printf("total           %lfms\n", ms_delta(&start, &codegen_end));
 
     return 1;
 }
